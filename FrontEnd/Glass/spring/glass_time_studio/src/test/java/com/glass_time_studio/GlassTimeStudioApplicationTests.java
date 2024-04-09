c58e0c6421bccecce586413f95bf61c4181b90fb
Dvdhan <han2041126@gmail.com>
2024-04-09 22:00:13 +0900

    디렉토리 구조 변경

---
+++ FrontEnd/Glass/spring/glass_time_studio/src/test/java/com/glass_time_studio/GlassTimeStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glass_time_studio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlassTimeStudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
